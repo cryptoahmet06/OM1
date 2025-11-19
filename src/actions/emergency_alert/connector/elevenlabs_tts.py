f851518b1a8dc8917543befe8d2ce4f80074c82e
Ahmet Yıldız <cryptoahmet06@gmail.com>
2025-11-19 10:30:23 +0000

    Fix imports and formatting for CI

--- src/actions/emergency_alert/connector/elevenlabs_tts.py
+++ src/actions/emergency_alert/connector/elevenlabs_tts.py
@@ -23,7 +23,6 @@ from zenoh_msgs import (
 # unstable / not released
 # from zenoh.ext import HistoryConfig, Miss, RecoveryConfig, declare_advanced_subscriber
 class EmergencyAlertElevenLabsTTSConnector(ActionConnector[EmergencyAlertInput]):
-
     def __init__(self, config: ActionConfig):
         super().__init__(config)
 
